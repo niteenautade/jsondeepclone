ae71a026af81f243095f76314ebe8c5b80800ff0
niteenautade <niteenautade@gmail.com>
2018-05-26 10:19:40 +0530

    json deep clone

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var jsonDeepClone = function (source){
+  var copy = {}
+  if(typeof(source)=="object" && !Array.isArray(source)){
+    Object.keys(source).forEach(function(key){
+        if(typeof(source[key])=="object"){
+            if(!Array.isArray(source[key])){
+              copy[key] = deepClone(source[key])
+            }
+            else{
+              copy[key] = source[key].map(function(a){return a})
+            }
+        }
+        else{
+             copy[key] = source[key]
+        }
+        
+    })  
+  }
+  return copy
+}
+module.exports = jsonDeepClone
